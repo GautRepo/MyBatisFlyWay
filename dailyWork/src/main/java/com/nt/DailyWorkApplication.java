457e1ed340240fb16d9f0440bdbaf717d9f92bdf
GautRepo <gautam.kr0352@gmail.com>
2024-02-27 11:21:06 +0530

    Implemented in memory cache

--- dailyWork/src/main/java/com/nt/DailyWorkApplication.java
+++ dailyWork/src/main/java/com/nt/DailyWorkApplication.java
@@ -5,10 +5,12 @@ import org.apache.ibatis.type.MappedTypes;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @MapperScan("com.nt.mapper")
 //@MappedTypes(StudentEntity.class)
+@EnableCaching
 public class DailyWorkApplication {
 
 	public static void main(String[] args) {
--- dailyWork/src/main/java/com/nt/service/StudentService.java
+++ dailyWork/src/main/java/com/nt/service/StudentService.java
@@ -2,32 +2,56 @@ package com.nt.service;
 
 import com.nt.entity.StudentEntity;
 import com.nt.mapper.StudentMapper;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
+
 
 @Service
+@Slf4j
+@CacheConfig(cacheNames = "student")
 public class StudentService {
 
     @Autowired
     private StudentMapper studentMapper;
 
 
+    @CachePut(key = "#studentEntity.sid"/*,cacheNames = "student"*/)
     public Integer saveStudent(StudentEntity studentEntity){
+        log.info("******call went to db*******");
        return studentMapper.saveStudent(studentEntity);
     }
 
+
+    @Cacheable/*(cacheNames = "student")*/
     public List<StudentEntity> getAllStudent() {
+        log.info("******call went to db*******");
         return studentMapper.getAllStudent();
     }
 
+    @Cacheable(key="#id")
     public StudentEntity getStudent(Integer id) {
+        log.info("******call went to db*******");
         return studentMapper.getStudent(id);
     }
 
+    @CacheEvict(key = "#sid")
     public String deleteStdById(Integer sid) {
-        return studentMapper.deleteById(sid) +" id has been deleted";
+        log.info("******call went to db*******");
+        return studentMapper.deleteById(sid) +" row has been deleted";
     }
+
+    /*@PostConstruct
+    public void initDB(){
+        IntStream.rangeClosed(4,1000)
+                .mapToObj(i-> new StudentEntity().toBuilder().sid(i).sname("abc"+i).sadd("Patna-"+i).sfee(i*10).build())
+                .forEach(s -> studentMapper.saveStudent(s));
+    }*/
 }
